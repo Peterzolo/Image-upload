bd95e1437581f826ceb840ac4e148c8a17836f52
Peter Solomon <user.email>
2022-02-23 12:43:35 +0100

    Added storage and filter functions

--- src/component/product/product.controller.js
+++ src/component/product/product.controller.js
@@ -1,5 +1,24 @@
-
+const multer = require('multer')              // multer will be used to handle the form data.
+const Aws = require('aws-sdk')   
 const Product = require('./product.model');
+
+
+const storage = multer.memoryStorage({
+    destination: function (req, file, cb) {
+        cb(null, '')
+    }
+})
+
+
+const filefilter = (req, file, cb) => {
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/jpg') {
+        cb(null, true)
+    } else {
+        cb(null, false)
+    }
+}
+
+
 
 
 exports.createProduct = async (req, res) =>{
