740a300985451e78fd0c77f87a56891bfb900c69
Peter Solomon <user.email>
2022-02-23 12:19:32 +0100

    Added product model

---
+++ src/component/product/product.model.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const ProductSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      default: "",
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Product = mongoose.model("product", ProductSchema);
+
+module.exports = Product;
