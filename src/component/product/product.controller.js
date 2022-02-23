7915488f3483c3ffc68f092cee6303ce0dc98781
Peter Solomon <user.email>
2022-02-23 12:51:15 +0100

    Added functions to filter and store image

--- src/component/product/product.controller.js
+++ src/component/product/product.controller.js
@@ -1,26 +1,28 @@
-const multer = require('multer')              // multer will be used to handle the form data.
-const Aws = require('aws-sdk')   
-const Product = require('./product.model');
-
+const multer = require("multer"); // multer will be used to handle the form data.
+const Aws = require("aws-sdk");
+const Product = require("./product.model");
 
 const storage = multer.memoryStorage({
-    destination: function (req, file, cb) {
-        cb(null, '')
-    }
-})
-
+  destination: function (req, file, cb) {
+    cb(null, "");
+  },
+});
 
 const filefilter = (req, file, cb) => {
-    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/jpg') {
-        cb(null, true)
-    } else {
-        cb(null, false)
-    }
-}
+  if (file.mimetype === "image/jpeg" || file.mimetype === "image/jpg") {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
 
+const upload = multer({ storage: storage, fileFilter: filefilter });
 
+const s3 = new Aws.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_ACCESS_KEY_SECRET,
+});
 
-
-exports.createProduct = async (req, res) =>{
-    res.send('request works')
-}
+exports.createProduct = async (req, res) => {
+  res.send("request works");
+};
