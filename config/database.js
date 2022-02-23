ecf1302f0da0b1a1374fb9eade5345043ed1ee70
Peter Solomon <user.email>
2022-02-23 11:59:37 +0100

    Initial commit

---
+++ config/database.js
@@ -0,0 +1,12 @@
+
+
+const mongoose = require('mongoose');
+
+const dbConnection = async () =>{
+    const mongoDbConn = await mongoose.connect(process.env.MONGO_URI)
+    console.log('MongoDb successfully connected')
+}
+
+
+
+module.exports = dbConnection;
